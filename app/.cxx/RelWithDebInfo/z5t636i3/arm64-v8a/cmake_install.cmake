4c10181627d0f38e7dcd3e27ca52b81d75613d7c
Roberto Ricotti <robertoricotti@outlook.com>
2026-06-10 14:01:34 +0200

    4.1.10 before release 4.1.11 da finire addpattern

--- app/.cxx/RelWithDebInfo/z5t636i3/arm64-v8a/cmake_install.cmake
+++ app/.cxx/RelWithDebInfo/z5t636i3/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Development/MC_V4/app/src/main/cpp
+# Install script for directory: D:/Developement/MC_V4/app/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Development/MC_V4/app/.cxx/RelWithDebInfo/z5t636i3/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Developement/MC_V4/app/.cxx/RelWithDebInfo/z5t636i3/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
